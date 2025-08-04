63ce77ff0279d427c6e3ca0f4ae92a5966ad3731
Francisco Gambino <Francisco.Gambino@transcore.com>
2025-08-04 09:55:40 -0300

    changed location of REDME.md

--- WordFinderProject/obj/Debug/net8.0/WordFinderProject.AssemblyInfo.cs
+++ WordFinderProject/obj/Debug/net8.0/WordFinderProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WordFinderProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f3911291432db28a6305151231079255531b77")]
 [assembly: System.Reflection.AssemblyProductAttribute("WordFinderProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WordFinderProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
